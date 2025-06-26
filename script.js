86b72a180c6fba1079b1232899d2a10b7b920d54
SkemaBC <skemabroadcasting@gmail.com>
2025-06-26 16:02:48 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ const navMenu = document.getElementById('navMenu');
 hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('show');
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const popup = document.getElementById("popup");
+  const popupImg = document.getElementById("popup-img");
+  const closeBtn = document.querySelector(".close-btn");
+
+  // Klik gambar dokumentasi
+  document.querySelectorAll(".item-dokumentasi img").forEach(img => {
+    img.addEventListener("click", () => {
+      popup.style.display = "flex";
+      popupImg.src = img.src;
+      popupImg.alt = img.alt;
+    });
+  });
+
+  // Tutup popup saat klik tombol
+  closeBtn.addEventListener("click", () => {
+    popup.style.display = "none";
+    popupImg.src = "";
+  });
+});
